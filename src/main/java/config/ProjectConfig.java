13f0ae99a684a3fefa399f2ad6b32e91a4593879
Демонов Евгений Васильевич <edemonov@dev.vtb.ru>
2024-02-16 16:02:09 +0300

    feat(3): Bean Predicate

--- src/main/java/config/ProjectConfig.java
+++ src/main/java/config/ProjectConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.Date;
+import java.util.function.Predicate;
 
 @Configuration
 public class ProjectConfig {
@@ -20,4 +21,11 @@ public class ProjectConfig {
     {
         return new Date();
     }
+
+    @Bean
+    Predicate<Integer> predicate()
+    {
+        return integer -> integer>=2&&integer<=5;
+    }
+
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,14 +4,23 @@ import config.ProjectConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.util.Date;
+import java.util.function.Predicate;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
+
 public class Main {
+
     public static void main(String[] args) {
         var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
 
         System.out.println(context.getBean(String.class));
         System.out.println(context.getBean(Date.class));
+
+        Predicate p=context.getBean(Predicate.class);
+        for (int i = 0; i < 10; i++) {
+
+        System.out.println("Протестируем  предикат : " +i + " preducate = " + p.test(i));
+        }
     }
 }
